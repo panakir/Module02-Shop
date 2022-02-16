3cf31ff7c1aa40bdddfaa1d59a29d6eb2090c9ba
ponokir <ponokir@gmail.com>
2022-02-16 20:02:59 +0300

    add catalog section and styles for it

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+import './index.html'
 import 'normalize.css'
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -8,6 +8,23 @@ import './styles/main.scss'
 import Swiper, { Navigation, Pagination } from 'swiper';
 
 var swiper = new Swiper(".offerSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    modules: [Navigation, Pagination],
+  });
+
+  var swiper = new Swiper(".trendsSwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
     slidesPerGroup: 3,
